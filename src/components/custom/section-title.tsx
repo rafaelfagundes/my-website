c43708154b9878f389326afb895cf3df62d4b95c
Rafael Fagundes <rafaelcflima@gmail.com>
2024-05-16 16:07:14 -0400

    fix: Update SectionTitle component to handle missing color prop

--- src/components/custom/section-title.tsx
+++ src/components/custom/section-title.tsx
@@ -8,7 +8,10 @@ function SectionTitle({
   return (
     <div className="text-3xl lg:text-4xl xl:text-5xl font-bold text-foreground mb-4 w-fit">
       <h2>{children}</h2>
-      <div className="h-2 w-ful mt-3" style={{ backgroundColor: color }}></div>
+      <div
+        className="h-2 w-ful mt-3"
+        style={{ backgroundColor: color || "#000000" }}
+      ></div>
     </div>
   );
 }
